7caf846f7540ba82d2c78e1913002955e169ed09
Karelyn Rojas <knrojas@ucdavis.edu>
2023-08-20 02:05:09 -0700

    split out route and controllers for user template

---
+++ controllers/userControllers.js
@@ -0,0 +1,37 @@
+const { User, Thought } = require("../models");
+
+module.exports = {
+    // Get all users
+    getAllUsers(req, res) {
+
+    },
+
+    // Get user by id
+    getUserById(req, res) {
+
+    },
+
+    // Create a new user
+    createUser(req, res) {
+    },
+
+    // Update a user by id
+    updateUser(req, res) {
+
+    },
+
+    // Delete a user by id
+    deleteUser(req, res) {
+
+    },
+
+    // Add a friend to a user's friend list
+    addFriend(req, res) { 
+
+    },
+
+    // Delete a friend from a user's friend list
+    deleteFriend(req, res) { 
+        
+    }
+}
